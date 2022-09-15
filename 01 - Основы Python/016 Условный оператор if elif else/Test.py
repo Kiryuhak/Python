6cc0901f0136e53f9fba3a69a13256ec367cefd1
Kiryuhak <arm2402@yandex.ru>
2022-09-15 14:21:31 +0500

    Python

---
+++ 01 - Основы Python/016 Условный оператор if elif else/Test.py
@@ -0,0 +1,27 @@
+x = 3
+
+if x > 3:
+    print('x > 3')
+elif x < 3:
+    print('x < 3')
+else:
+    print('x == 3')
+
+day_time = 'afternoon'
+
+if day_time == 'morning':
+    print('Monster wakes up')
+elif day_time == 'afternoon':
+    print('Monster is walking')
+elif day_time == 'evening':
+    print('Monster is eating')
+elif day_time == 'night':
+    print('Monster is sleeping')
+else:
+    print('Monster is doing something')
+
+
+user_input = input('Input something...')
+
+if user_input == 'Hello':
+    print('Hello, Nice to meet you')
---
+++ 01 - Основы Python/017 Цикл for/Test.py
@@ -0,0 +1,8 @@
+number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# for number in number_list:
+#     print(str(number) + ' Hola!')
+
+for number in number_list:
+    if number % 2 == 0:
+        print(number)
